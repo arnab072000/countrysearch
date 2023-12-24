749acf91994bc521298b5c4d4971f0ffa9e0e583
arnab072000 <arnab11kr@gmail.com>
2023-12-24 11:43:51 +0530

    "initial commit"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,117 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import './App.css';
+//import Country from "./components/Country"
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const headerStyle = {
+  position: 'fixed',
+  top: '0',
+  left: '0',
+  width: '100%',
+  backgroundColor: '#f8f8f8',
+  padding: '10px',
+  boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+  zIndex: '1000',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const searchStyle = {
+  width: '50%',
+  padding: '10px',
+  fontSize: '16px',
+};
+
+const containerStyle = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '80px 20px',
+};
+
+const cardStyle = {
+  width: '200px',
+  border: '1px solid #ccc',
+  borderRadius: '10px',
+  margin: '10px',
+  padding: '10px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+const imageStyle = {
+  width: '100px',
+  height: '100px',
+};
+
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error('Error fetching data: ', error));
+  }, []);
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={headerStyle}>
+        <input
+          type="text"
+          placeholder="Search for countries..."
+          style={searchStyle}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div style={containerStyle}>
+        {filteredCountries.map((country) => (
+          <div key={country.cca3} style={cardStyle}>
+            <img
+              src={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+              style={imageStyle}
+            />
+            <h2>{country.name.common}</h2>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
